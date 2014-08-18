53c8ffaea1fa44dfc8789392bcbcfae0004afdde
Sam Gong <gongfan99@hotmail.com>
2014-08-17 23:31:17 -0400

    greyscale image from camera issue solved; first image from camera is often NULL causing cvCvtColor function to fail

--- source/ImageProduceCamera.c
+++ source/ImageProduceCamera.c
@@ -8,14 +8,12 @@ extern "C" {
 ImageProduceCamera* ImageProduceCamera_create(){
 	ImageProduceCamera* this_ptr = (ImageProduceCamera*)malloc(sizeof(ImageProduceCamera));
 	this_ptr->ops = (ImageProduceCamera_ops*)malloc(sizeof(ImageProduceCamera_ops));
-	//this_ptr->capture = cvCaptureFromCAM(CV_CAP_ANY); //Capture using any camera connected to your system
-	this_ptr->capture = cvCaptureFromFile("C:/Users/Public/Videos/Sample Videos/Wildlife.wmv");
+	this_ptr->capture = cvCaptureFromCAM(0); //CV_CAP_ANY : Capture using any camera connected to your system
+	//this_ptr->capture = cvCaptureFromFile("C:/Users/Public/Videos/Sample Videos/Wildlife.wmv");
 
 	CvSize size;
 	size.width = (int)cvGetCaptureProperty(this_ptr->capture, CV_CAP_PROP_FRAME_WIDTH);
 	size.height = (int)cvGetCaptureProperty(this_ptr->capture, CV_CAP_PROP_FRAME_HEIGHT);
-	printf("%f \n", cvGetCaptureProperty(this_ptr->capture, CV_CAP_PROP_FORMAT));
-	printf("%d %d \n", size.width, size.height);
 	((ImageProduce*)this_ptr)->data = cvCreateImage(size,IPL_DEPTH_8U,1); //reserve place for greyscale image
 	
 	*(this_ptr->ops) = (ImageProduceCamera_ops){
@@ -27,19 +25,15 @@ ImageProduceCamera* ImageProduceCamera_create(){
 
 void ImageProduceCamera_process(ImageProduceCamera* this_ptr){
 	IplImage* frame;
-	frame = cvQueryFrame(this_ptr->capture); //Create image frames from capture
-	int i = frame->depth;
-	int j = frame->nChannels;
-	printf("frame format: %d %d\n", i, j);
-	//printf("I am here.");
+	do {
+		frame = cvQueryFrame(this_ptr->capture); //capture an image
+	} while (frame == NULL); //first frame from camera is often NULL
 	cvCvtColor(frame, ((ImageProduce*)this_ptr)->data, CV_BGR2GRAY); //convert RGB to grey
-	((ImageProduce*)this_ptr)->data = frame;
-	//printf("I am here.");
 }
 
 void ImageProduceCamera_destroy(ImageProduceCamera* this_ptr){
 	cvReleaseCapture(&(this_ptr->capture)); //Release capture.
-	//cvReleaseImage(&(((ImageProduce*)this_ptr)->data));
+	cvReleaseImage(&(((ImageProduce*)this_ptr)->data));
 	free(this_ptr->ops);
 	free(this_ptr);
 }
