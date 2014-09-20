f1821ec48e428cfd0e43871e2defdf8a17e5c656
Sam Gong <gongfan99@hotmail.com>
2014-09-19 23:22:51 -0400

    code::blocks setup successfully

--- test/ImageDecoderQR/build/CMakeFiles/try.dir/cmake_clean.cmake
+++ test/ImageDecoderQR/build/CMakeFiles/try.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
   "CMakeFiles/try.dir/ImageDecoderQR_test.cpp.obj"
-  "CMakeFiles/try.dir/C_/Users/Sam/Documents/User/Embeded/ServerApp/webserver/source/ImageProduceCamera.c.obj"
-  "CMakeFiles/try.dir/C_/Users/Sam/Documents/User/Embeded/ServerApp/webserver/source/DirectCamera.cpp.obj"
-  "CMakeFiles/try.dir/C_/Users/Sam/Documents/User/Embeded/ServerApp/webserver/source/ImageDecoderQR.cpp.obj"
-  "CMakeFiles/try.dir/C_/Users/Sam/Documents/User/Embeded/ServerApp/webserver/source/utils.cpp.obj"
+  "CMakeFiles/try.dir/C_/Users/sam/Documents/User/Embeded/ServerApp/webserver/source/ImageProduceCamera.c.obj"
+  "CMakeFiles/try.dir/C_/Users/sam/Documents/User/Embeded/ServerApp/webserver/source/DirectCamera.cpp.obj"
+  "CMakeFiles/try.dir/C_/Users/sam/Documents/User/Embeded/ServerApp/webserver/source/ImageDecoderQR.cpp.obj"
+  "CMakeFiles/try.dir/C_/Users/sam/Documents/User/Embeded/ServerApp/webserver/source/utils.cpp.obj"
   "try.pdb"
   "try.exe"
   "try.exe.manifest"
